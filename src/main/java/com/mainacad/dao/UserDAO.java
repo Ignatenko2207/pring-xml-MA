3643c240c4721a06faff0c254d6d26f339c621b2
Alex <ignatenko2207@gmail.com>
2019-09-05 19:26:45 +0300

    Initial commit

---
+++ src/main/java/com/mainacad/dao/UserDAO.java
@@ -0,0 +1,62 @@
+package com.mainacad.dao;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Session;
+import com.mainacad.dao.connection.ConnectionFactory;
+import com.mainacad.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Setter
+@Getter
+//@Component
+@Repository
+@Qualifier("userDAO")
+public class UserDAO{
+
+    @Autowired
+//    @Qualifier("h2Factory")
+	private ConnectionFactory connectionFactory;
+	
+	public void saveOrUpdate(User user){
+		Session session = connectionFactory.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        session.saveOrUpdate(user);
+        session.getTransaction().commit();
+        session.close();		
+	}
+	
+	public User findOne(Integer id){
+		Session session = connectionFactory.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        User userFromDB = (User)session.find(User.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return userFromDB;
+	}
+
+	public List<User> findAll(){
+		Session session = connectionFactory.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        
+        String sql = "SELECT * FROM users";
+              
+        List<User> result = session.createNativeQuery(sql).getResultList();
+        
+        session.close();
+        return result;
+	}
+	
+	public void delete(User user) {
+		Session session = connectionFactory.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+	}
+	
+}
